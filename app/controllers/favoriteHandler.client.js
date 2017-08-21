6405f7ddbb50ebd12c96b2a325d799be26165e06
SCA2 <tpryan_1965@yahoo.com>
2017-08-21 14:58:21 -0700

    reload images after attaching handler

--- app/controllers/favoriteHandler.client.js
+++ app/controllers/favoriteHandler.client.js
@@ -4,16 +4,20 @@
 
   var favorites = document.querySelectorAll('.favorite');
 
-  
+  function cacheBuster(url) {
+    return url.replace(/\?cache=\d*/, "") + "?cache=" + new Date().getTime().toString();
+  }
+
   function favoriteUrl(favorite) {
     return window.location.href + '/' + favorite.id + '/likes';
   }
 
   ajaxFunctions.ready(() => {
     favorites.forEach(favorite => {
+
       let image = favorite.querySelector('img.card-img-top');
+
       image.addEventListener('error', function(e) {
-        console.log(e);
         image.src = 'https://lorempixel.com/295/325/abstract/'
       }, true);
 
@@ -23,6 +27,12 @@
       };
       ajaxFunctions.ajaxRequest('GET', favoriteUrl(favorite), favorite.updateLikesCount);
     })
+
+    let images = document.querySelectorAll('img.card-img-top');
+
+    for(let i = 0; i < images.length; i++) {
+      images[i].src = cacheBuster(images[i].src);
+    };
   });
 
   favorites.forEach((favorite) => {
